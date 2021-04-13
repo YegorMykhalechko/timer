73c4626086797058548f52adc1e75c1f84d1ca4e
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2021-04-13 21:50:32 +0200

    classTimer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
 'use strict'
-import './styles.css';
-import './js/task1.js';
-import './js/task2.js';
-import './js/task3.js';
+// import './styles.css';
+class CountdownTimer{
+    constructor(selector, targetDate) {
+        this.selector = document.querySelector(selector);
+        this.targetDate = targetDate;
+        this.time = 
+    }
+}
 
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2019'),
+  });
+
+/*
+ * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+ * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+ */
+const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+/*
+ * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+ * остатка % и делим его на количество миллисекунд в одном часе
+ * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+ */
+const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+/*
+ * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+ * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+ */
+const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+/*
+ * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+ * миллисекунд в одной секунде (1000)
+ */
+const secs = Math.floor((time % (1000 * 60)) / 1000);
 
